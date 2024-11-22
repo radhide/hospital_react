b5f841eedc911a85c7e116e60283ef1018ca1663
9bytes <9bytes@gmail.com>
2024-11-22 17:50:48 +0530

    code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,15 +7,39 @@ const Footer = () => {
       <div className="footerColumn">
         <div>
           ADDRESS
+          <div className="subColumn">
+            <div>Location</div>
+            <div>call +01 1234567890</div>
+            <div>demo@gmail.com</div>
+            <div>icons</div>
+          </div>
         </div>
         <div>
           USEFUL LINK
+          <div className="subColumn">
+            <div>Location</div>
+            <div>call +01 1234567890</div>
+            <div>demo@gmail.com</div>
+            <div>icons</div>
+          </div>
         </div>
         <div>
           LATEST POSTS
+          <div className="subColumn">
+            <div>Location</div>
+            <div>call +01 1234567890</div>
+            <div>demo@gmail.com</div>
+            <div>icons</div>
+          </div>
         </div>
         <div>
           NEWS
+          <div className="subColumn">
+            <div>Location</div>
+            <div>call +01 1234567890</div>
+            <div>demo@gmail.com</div>
+            <div>icons</div>
+          </div>
         </div>
       </div>
     </div>
